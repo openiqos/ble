0187803d2f3e312a38f4a62e37b2dc7fa47e6e4c
Misha Marinenko <0x77dev@protonmail.com>
2020-02-09 17:29:09 +0200

    0.0.1: Connecting to device.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export declare class iQOS {
-    constructor();
+    private device;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 export class iQOS {
-    constructor() {
+    private bluetooth: Bluetooth;
+    public device: (BluetoothDevice | undefined) = undefined;
+    constructor(bluetooth: Bluetooth) {
+        this.bluetooth = bluetooth;
+    }
 
+    async connect() {
+        try {
+            this.device = await this.bluetooth.requestDevice({
+                acceptAllDevices: true,
+                filters: [{
+                    services: ["daebb240-b041-11e4-9e45-0002a5d5c51b"]
+                }]
+            });
+        } catch (error) {
+            if (error) throw new Error("Error while connecting to device: " + error.toString());
+        }
     }
 }
